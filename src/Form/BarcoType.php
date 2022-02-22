80b1aad0c5d6ab5a259961fce7110b4378efca62
pepeluestech <92080663+jbersan99@users.noreply.github.com>
2022-02-22 21:23:18 +0100

    Last changes

--- src/Form/BarcoType.php
+++ src/Form/BarcoType.php
@@ -11,21 +11,90 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class BarcoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre', TextType::class)
-            ->add('matricula' , TextType::class)
-            ->add('pasajeros_maximos', IntegerType::class)
-            ->add('precio_con_patron', IntegerType::class)
-            ->add('precio_sin_patron', IntegerType::class)
-            ->add('eslora' , IntegerType::class)
-            ->add('calado' , IntegerType::class)
-            ->add('caballos' , IntegerType::class)
-            ->add('licencia', TextType::class)
+            ->add('nombre', TextType::class ,[
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor introduce tu nombre',
+                    ])
+                ],
+            ])
+            ->add('matricula' , TextType::class ,[
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor introduce tu nombre',
+                    ])
+                ],
+            ])
+            ->add('pasajeros_maximos', IntegerType::class ,[
+                'constraints' => [
+                    new Length([
+                        'min' => 1,
+                        'minMessage' => 'El maximo de pasajeros es {{ limit }}',
+                        'max' => 20,
+                    ]),
+                ],
+            ])
+            ->add('precio_con_patron', IntegerType::class ,[
+                'constraints' => [
+                    new Length([
+                        'min' => 400,
+                        'minMessage' => 'El maximo de precio con patrón establecido es {{ limit }}',
+                        'max' => 20000,
+                    ]),
+                ],
+            ])
+            ->add('precio_sin_patron', IntegerType::class ,[
+                'constraints' => [
+                    new Length([
+                        'min' => 350,
+                        'minMessage' => 'El maximo de precio sin patrón establecido es {{ limit }}',
+                        'max' => 18000,
+                    ]),
+                ],
+            ])
+            ->add('eslora', IntegerType::class ,[
+                'constraints' => [
+                    new Length([
+                        'min' => 1,
+                        'minMessage' => 'El maximo de eslora es {{ limit }}',
+                        'max' => 20,
+                    ]),
+                ],
+            ])
+            ->add('calado', IntegerType::class ,[
+                'constraints' => [
+                    new Length([
+                        'min' => 1,
+                        'minMessage' => 'El maximo de calado es {{ limit }}',
+                        'max' => 20,
+                    ]),
+                ],
+            ])
+            ->add('caballos', IntegerType::class ,[
+                'constraints' => [
+                    new Length([
+                        'min' => 1,
+                        'minMessage' => 'El maximo de caballos es {{ limit }}',
+                        'max' => 20,
+                    ]),
+                ],
+            ])
+            ->add('licencia', TextType::class ,[
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor introduce tu nombre',
+                    ])
+                ],
+            ])
             ->add('latitud', IntegerType::class)
             ->add('longitud', IntegerType::class)
             ->add('patron', CheckboxType::class, [
